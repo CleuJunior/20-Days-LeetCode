56f3775b9e5d11191e3d9e684c4bc66a89f09361
Cleonildo Jr <cleu.junior@gmail.com>
2022-07-24 18:10:50 -0300

    Reverse String

---
+++ ReverseString/src/test/SolutionTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import main.java.Solution;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SolutionTest {
+    private Solution solution = new Solution();
+    private char[] arrayChar = new char[]{'h','e','l','l','o'};
+
+    @Before
+    public void init() {
+    }
+
+    @Test
+    public void exampleTest01() {
+
+    }
+
+}
